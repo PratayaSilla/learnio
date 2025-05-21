8683c0f88f702aaf6bfebf4e7baaeac45b30f0f0
Prataya <pratayasilla@gmail.com>
2025-05-22 01:23:58 +0530

    not_found_fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,13 @@
-// import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import LandingPage from './Pages/LandingPage/LandingPage';
-// import Authentication from './Pages/Authentication/Authentication';   
-// import Layout from './Layout';
 import App from './App';
-
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Layout />,
-//     children: [
-//       {
-//         path:'',
-//         element:<LandingPage/> 
-//       },
-//       {
-//         path:'/auth',
-//         element:<Authentication/>
-
-//       }
-//     ]
-//   }
-// ])
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <RouterProvider router={router} />  */}
-    <App />
-    </StrictMode> 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
- 
