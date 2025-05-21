7237e5bbd3b012ebccbcf8ef42feb32bff3049e3
Prataya <pratayasilla@gmail.com>
2025-05-22 01:31:11 +0530

    Error Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './Pages/LandingPage/LandingPage'
-import Login from './components/Login/Login'
-import SignUp from './components/SignUp/SignUp'
-import DashboardPage from './Pages/Dashboard-Main/DashboardPage'
-import DashboardHome from './components/SignUp/Dashboard/Dashboard-Home/Dashboard-Home'
-import DashboardChapters from './components/SignUp/Dashboard/Dashboard-Chapters/Dashboard-Chapters'
-import QuizAttempt from './components/SignUp/Dashboard/Dashboard-Quizzes/QuizAttempt/QuizAttempt'
-import ChooseCourses from './components/Choose-Courses/Choose-courses'
-import ChapterVideo from './components/SignUp/Dashboard/Dashboard-Chapters/ChapterVideo/ChapterVideo'
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage/LandingPage';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import DashboardPage from './Pages/Dashboard-Main/DashboardPage';
+import QuizAttempt from './components/SignUp/Dashboard/Dashboard-Quizzes/QuizAttempt/QuizAttempt';
+import ChooseCourses from './components/Choose-Courses/Choose-courses';
+import ChapterVideo from './components/SignUp/Dashboard/Dashboard-Chapters/ChapterVideo/ChapterVideo';
 
 function App() {
   return (
-    <>
-    <div>
-    </div>
-    <BrowserRouter>
-        <Routes> 
-            <Route path='/' element={<LandingPage />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/choosecourses' element={<ChooseCourses />} />
-            <Route path='/dashboard' element={<DashboardPage />} />
-            <Route path='/attemptquiz' element={<QuizAttempt />} />
-            <Route path='/chaptervideo' element={<ChapterVideo />} />
-
-        </Routes>
-    </ BrowserRouter>
-    </>
-  )
+    <Routes> 
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/choosecourses' element={<ChooseCourses />} />
+      <Route path='/dashboard' element={<DashboardPage />} />
+      <Route path='/attemptquiz' element={<QuizAttempt />} />
+      <Route path='/chaptervideo' element={<ChapterVideo />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
+
