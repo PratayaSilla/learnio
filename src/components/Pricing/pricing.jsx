c2112f2337d61e59da47e6962fc912b7f107f891
Prataya <pratayasilla@gmail.com>
2025-05-22 01:09:32 +0530

    typo 2

--- src/components/Pricing/pricing.jsx
+++ src/components/Pricing/pricing.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Pricing.css';
+import './pricing.css';
 
 const Pricing = () => {
     const navigate = useNavigate();
