3b82fcb781936284d92ba58da9d5736a22dbb390
Prataya <pratayasilla@gmail.com>
2025-05-22 01:12:25 +0530

    typo 3 (doing for deploys)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import './footer.css';
+import './Footer.css';
 
 const Footer = () => {
   const navigate = useNavigate(); 
