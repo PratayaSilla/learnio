9007c4c59e04f0fa969d677ab41eba81e32bf222
Prataya <pratayasilla@gmail.com>
2025-05-22 01:08:08 +0530

    typo fix

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './testimonials.css';
+import './Testimonials.css';
 
 const Testimonials = () => {
   const navigate = useNavigate();
